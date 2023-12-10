b28c32f35b4db93970a63dfc9192be1285d81aa9
Nikita Bijwe <91217413+nikitabijwe@users.noreply.github.com>
2023-12-10 22:59:34 +0530

    Time: 60 ms (65.26%), Space: 42.8 MB (43.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,23 +5,16 @@
  */
 var twoSum = function(nums, target) {
     // Create a hash table to store the indices of numbers
-    const numIndices = {};
-
-    // Iterate through the array
-    for (let i = 0; i < nums.length; i++) {
-        // Calculate the complement (the number needed to reach the target)
-        const complement = target - nums[i];
-
-        // Check if the complement exists in the hash table
-        if (numIndices.hasOwnProperty(complement)) {
-            // If found, return the indices of the two numbers
+    const numIndices={};
+    
+    for(let i=0; i<nums.length ; i++){
+        let complement = target - nums[i];
+        
+        if (numIndices.hasOwnProperty(complement)){
             return [numIndices[complement], i];
         }
-
-        // If not found, store the current number and its index in the hash table
-        numIndices[nums[i]] = i;
-    }
-
-    // If no solution is found, return an empty array or handle it as needed
-    return [];
-};
+        numIndices[nums[i]]=i;
+            
+        }
+        return [];
+    };
