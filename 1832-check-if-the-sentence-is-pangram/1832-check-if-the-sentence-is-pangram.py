b99735003784dff49825af8ebcb0f048fa9feef4
Nikita Bijwe <91217413+nikitabijwe@users.noreply.github.com>
2023-12-24 15:35:46 -0500

    Time: 13 ms (69.39%), Space: 13.4 MB (19.61%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -5,8 +5,21 @@ class Solution(object):
         :rtype: bool
         """
         
-        seen = set(sentence)
+#         seen = set(sentence)
         
-        if len(seen)==26:
-            return True
-        return False
+#         if len(seen)==26:
+#             return True
+#         return False
+
+
+
+        # We iterate over 'sentence' for 26 times, one for each letter 'curr_char'.
+        for i in range(26):
+            curr_char = chr(ord('a') + i)
+
+            # If 'sentence' doesn't contain 'curr_char', it is not a pangram.
+            if sentence.find(curr_char) == -1:
+                return False
+        
+        # If we manage to find all 26 letters, it is a pangram.
+        return True
