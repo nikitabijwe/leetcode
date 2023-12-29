48e60f4da910c6a441408298c5ede52ef5bb2cb1
Nikita Bijwe <91217413+nikitabijwe@users.noreply.github.com>
2023-12-29 15:04:46 -0500

    Time: 78 ms (25.71%), Space: 14.1 MB (43.68%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,21 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        chars = Counter()
-
-        left = right = 0
-
-        res = 0
-        while right < len(s):
-            r = s[right]
-            chars[r] += 1
-
-            while chars[r] > 1:
-                l = s[left]
-                chars[l] -= 1
-                left += 1
-
-            res = max(res, right - left + 1)
-
-            right += 1
+        l=0
+        charSet=set()
+        res=0
+        
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l+=1
+            charSet.add(s[r])
+            res=max(res, r-l+1)
         return res
