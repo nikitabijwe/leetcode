66e1cb56280b0d87531ddf8f1feb60335598d1cd
Nikita Bijwe <91217413+nikitabijwe@users.noreply.github.com>
2024-01-08 21:11:16 -0500

    Time: 176 ms (34.25%), Space: 17.8 MB (5.52%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        counts=defaultdict(int)
+        ans=0
+        
+        for c in chars:
+            counts[c]+=1
+            
+        for word in words:
+            word_count=defaultdict(int)
+            for c in word:
+                word_count[c] +=1
+                
+            good=True
+            
+            for c, freq in word_count.items():
+                if counts[c] < freq:
+                    good=False
+                    break
+            if good:
+                ans+=len(word)
+                
+        return ans
+            
+        
+    
+                
