87dbc9c9163297c4a8accc0b26e18a349416575f
Nikita Bijwe <91217413+nikitabijwe@users.noreply.github.com>
2024-01-28 10:45:52 -0500

    Time: 37 ms (97.50%), Space: 17 MB (59.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,53 +1,18 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-#         if len(s)!=len(t):
-#             return false
-        
-#         s=sorted(s)
-#         t=sorted(t)
-        
-#         return s == t
-
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
         
-        dict={}
+        table = [0] * 26
         
         for char in s:
-            if char not in dict:
-                dict[char]=1
-            else:
-                dict[char]+=1
-                
+            table[ord(char) - ord('a')] +=1
+            
         for char in t:
-            if (char not in dict) or (dict[char]==0):
+            table[ord(char) - ord('a')] -=1
+            if table[ord(char) - ord('a')] < 0:
                 return False
-            dict[char]-=1
         return True
-        
-        
-        
-#         class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         if len(s) != len(t):
-#             return False
 
-#         word_count = {}
-#         for c in s:
-#             if c not in word_count:
-#                 word_count[c] = 1
-#             else:
-#                 word_count[c] += 1
-
-#         for c in t:
-#             if (c not in word_count) or (word_count[c] == 0):
-#                 return False
-#             word_count[c] -= 1
-
-#         return True
+            
+        
