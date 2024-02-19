dd0b71032540e1f1238da4e731114f3488472cbe
Nikita Bijwe <91217413+nikitabijwe@users.noreply.github.com>
2024-02-19 14:42:09 -0500

    Time: 100 ms (26.30%), Space: 22.4 MB (11.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,24 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-            
-#         dict=collections.defaultdict(list)
-        
-#         for ele in strs:
-#             dict[tuple(sorted(ele))].append(ele)
-#         return dict.values()
-
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans = collections.defaultdict(list)
         for s in strs:
             count = [0] * 26
             for c in s:
                 count[ord(c) - ord('a')] += 1
             ans[tuple(count)].append(s)
+
         return ans.values()
+        
+        
+# For "eat":
+# count: [1, 0, 0, ..., 1, 0, 0]
+# This count tuple becomes a key, and "eat" is appended to the corresponding list.
+# For "tea":
+
+# count: [1, 0, 0, ..., 1, 0, 0] (same as "eat")
+# Appended to the same list as "eat".
+# For "tan":
+
+# count: [1, 0, 1, ..., 0, 0, 0]
+# A different count tuple, so a new key is created, and "tan" is appended.
+# The same process continues for the remaining strings.
